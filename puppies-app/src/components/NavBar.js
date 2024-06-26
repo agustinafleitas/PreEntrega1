893d998a22839fbff7ef85eac9b100638ad3decd
agustinafleitas <agustinaafd@gmail.com>
2024-06-25 22:54:54 -0300

    style

--- puppies-app/src/components/NavBar.js
+++ puppies-app/src/components/NavBar.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import images from './images';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-export default function Navbar(){
+export default function PuppiesNavBar(){
     return(
         <div>
+            <Navbar bg="light" data-bs-theme="light">
             <div>
-                <img src={images.LogoCompleto} alt='Texto logo'></img>
+                <img src={images.LogoCompleto} alt='Texto logo' style={{ width: '150px'}}></img>
             </div>
-            <nav className='nav'>
-                <ul>
-                    <li><a href="/">Inicio</a></li>
-                    <li><a href="/">Productos</a></li>
-                    <li><a href="/">Sobre nosotros</a></li>
-                    <li><a href="/">Carrito</a></li>
-                </ul>
-            </nav>
+                <Container>
+                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
         </div>
     )
 }
+
+
